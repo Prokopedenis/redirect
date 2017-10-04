ca0f672a3bd74c6801a52a092edf3496f9f8e8dd
Prokopedenis <30890119+Prokopedenis@users.noreply.github.com>
2017-10-04 12:54:31 +0300

    Add files via upload

---
+++ ajax.php
@@ -0,0 +1,69 @@
+﻿<?php
+
+	try 
+	{
+		$dsn = "mysql:host=localhost;dbname=redir_url;charset=utf8";
+		$pdo = new PDO($dsn, "root", "vertrigo");
+	} 
+	catch (PDOException $error) 
+	{
+		print "Error!: " . $error->getMessage() . "<br/>";
+		die();
+	}
+	
+	//обработка входящей ссылки
+	if(isset($_GET["q"]))
+	{
+		$q = $_REQUEST["q"];
+
+		if(isset($_GET["e"])) //проверяем наличие желаемого вида для ссылки
+		{
+			$e = $_REQUEST["e"];
+			
+			$stmt=$pdo->query("SELECT COUNT(*) AS LLL FROM links l WHERE l.NewLink='".$e."'");
+			$row = $stmt->fetch();
+			if($row['LLL']==0) //если ссылка уже есть в базе, выводим соответсвующее сообщение
+			{
+				$pdo->query("INSERT links (NewLink, Link) VALUES ('".$e."', '".$q."')"); //добавляем в базу
+				$pdo->query("UPDATE counter c SET c.Counter=c.Counter+1"); //увеличиваем счётчик
+				
+				echo "<a href='http://localhost/$e' target='_blank'>http://localhost/$e</a>"; 
+			}
+			else
+			{
+				die("ссылка занята, введите другую");
+			}
+		}
+		else //случайная ссылка
+		{
+			$stmt = $pdo->query("SELECT l.Link,l.NewLink FROM links l WHERE l.Link='".$q."'");
+			$exist = $stmt->rowCount();
+			if($exist!=0) //если ссылка уже есть в базе, выводим готовое сокращение
+			{
+				while ($row = $stmt->fetch())
+				{
+					$newc=$row['NewLink'];
+				}
+				die("<a href='http://localhost/$newc' target='_blank'>http://localhost/$newc</a>");
+			}
+			
+			$stmt = $pdo->query("SELECT c.Counter FROM counter c"); //добываем счётчик
+			while ($row = $stmt->fetch())
+			{
+				$c=$row['Counter'];
+			}
+			
+			$newc=base_convert($c, 10, 36); //генерируем ссылку из счётчика
+			$c++; 
+			
+			$pdo->query("INSERT links (NewLink, Link) VALUES ('".$newc."', '".$q."')"); //добавляем в базу
+			$pdo->query("UPDATE counter c SET c.Counter=c.Counter+1"); //увеличиваем счётчик
+			
+			
+			echo "<a href='http://localhost/$newc' target='_blank'>http://localhost/$newc</a>"; 
+			
+		}	
+	}
+	
+	
+?>
---
+++ redir_url 20171004 1254.sql
@@ -0,0 +1,81 @@
+﻿--
+-- Скрипт сгенерирован Devart dbForge Studio for MySQL, Версия 7.2.78.0
+-- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
+-- Дата скрипта: 04.10.2017 12:54:03
+-- Версия сервера: 5.7.17
+-- Версия клиента: 4.1
+--
+
+
+-- 
+-- Отключение внешних ключей
+-- 
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+
+-- 
+-- Установить режим SQL (SQL mode)
+-- 
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- 
+-- Установка кодировки, с использованием которой клиент будет посылать запросы на сервер
+--
+SET NAMES 'utf8';
+
+-- 
+-- Установка базы данных по умолчанию
+--
+USE redir_url;
+
+--
+-- Описание для таблицы counter
+--
+DROP TABLE IF EXISTS counter;
+CREATE TABLE counter (
+  ID INT(11) NOT NULL AUTO_INCREMENT,
+  Counter INT(11) NOT NULL,
+  PRIMARY KEY (ID)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 2
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+ROW_FORMAT = DYNAMIC;
+
+--
+-- Описание для таблицы links
+--
+DROP TABLE IF EXISTS links;
+CREATE TABLE links (
+  ID INT(11) NOT NULL AUTO_INCREMENT,
+  NewLink VARCHAR(255) DEFAULT NULL,
+  Link VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (ID)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 1
+CHARACTER SET utf8
+COLLATE utf8_general_ci
+ROW_FORMAT = DYNAMIC;
+
+-- 
+-- Вывод данных для таблицы counter
+--
+INSERT INTO counter VALUES
+(1, 1000000000);
+
+-- 
+-- Вывод данных для таблицы links
+--
+
+-- Таблица redir_url.links не содержит данных
+
+-- 
+-- Восстановить предыдущий режим SQL (SQL mode)
+-- 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+
+-- 
+-- Включение внешних ключей
+-- 
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
